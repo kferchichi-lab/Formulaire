e404509baf06a67ffb9cf874a669c0b7ef8fc24c
kferchichi-lab <k.ferchichi@tpr.com.tn>
2026-05-15 15:24:43 +0100

    Update conducteur.py

--- conducteur.py
+++ conducteur.py
@@ -139,7 +139,7 @@ st.markdown("""
         }
     </style>
 """, unsafe_allow_html=True)
-            if submitted:
+                if submitted:
                 if not ref_filiere or not num_lopin:
                     st.error("Veuillez remplir les champs obligatoires (Filière et Lopin).")
                 else:
