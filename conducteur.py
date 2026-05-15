b3c26a28ec7120159775caf69393dc612d77357d
kferchichi-lab <k.ferchichi@tpr.com.tn>
2026-05-15 12:21:59 +0100

    conducteur.py

--- conducteur.py
+++ conducteur.py
@@ -265,10 +265,7 @@ with tab_stats:
     
     # Petit rappel des codes en dessous pour l'utilisateur
     st.info("**Rappel des codes :** **T** : Température | **H** : Hydraulique | **O** : Outillage | **R** : Raclage")
-            st.divider()
-            st.subheader("Total des minutes d'arrêt par cause")
-            fig2 = px.bar(df_filtered, x='Cause', y='Duree_Min', color='Presse', barmode='group', title="Durée totale des arrêts par cause (min)")
-            st.plotly_chart(fig2, use_container_width=True)
+         
     else:
         st.info("Enregistrez des données pour voir les graphiques.")
 
