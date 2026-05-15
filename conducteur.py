611ff8626c49add603963a034ed73f0d72a3f105
kferchichi-lab <k.ferchichi@tpr.com.tn>
2026-05-15 15:34:00 +0100

    Update conducteur.py

--- conducteur.py
+++ conducteur.py
@@ -22,7 +22,42 @@ st.markdown("""
     <style>
         header { visibility: visible !important; height: 60px !important; }
         .block-container { padding-top: 2rem !important; }
-        .temp-header { color: #0047AB; font-weight: bold; margin-bottom: 5px; }
+        .temp-header { color: #0047AB; font-weight: bold; margin-bottom: 5px; }*
+
+        <style>
+        /* On rend le header système visible pour la flèche mobile */
+        header {
+            visibility: visible !important;
+            height: 60px !important;
+        }
+        
+        /* --- CONFIGURATION PC (Par défaut) --- */
+        .block-container {
+            padding-top: 5rem !important; /* On augmente ici pour éviter le crop PC */
+            padding-bottom: 2rem !important;
+            padding-left: 5rem !important;
+            padding-right: 5rem !important;
+        }
+
+        /* --- CONFIGURATION SMARTPHONE --- */
+        @media (max-width: 768px) {
+            .block-container {
+                padding-top: 3.5rem !important; /* Un peu moins pour mobile pour garder votre 'très bon' rendu */
+                padding-left: 1.5rem !important;
+                padding-right: 1.5rem !important;
+            }
+            
+            [data-testid="stImage"] {
+                margin-top: 10px !important;
+            }
+        }
+
+        /* Sécurité pour l'image (Logo) */
+        [data-testid="stImage"] img {
+            max-width: 100%;
+            height: auto;
+            object-fit: contain !important;
+        }
         
         /* Bouton Enregistrer Premium */
         div.stButton > button {
