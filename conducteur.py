0f304572d67a55d81fe9462e5f6d06888365c1e8
kferchichi-lab <k.ferchichi@tpr.com.tn>
2026-05-15 14:33:34 +0100

    Update conducteur.py

--- conducteur.py
+++ conducteur.py
@@ -268,20 +268,9 @@ with tab_stats:
 
     # Ajouter les bordures blanches autour des barres pour mieux les distinguer
 # AJOUT DE L'EFFET AU SURVOL
-    fig.update_traces(
-        textinfo='percent',
-    # Effet : La part s'agrandit légèrement au survol (pull)
-        hoverinfo='label+percent+value',
-        marker=dict(line=dict(color='#000000', width=0)), # Pas de ligne par défaut
-    # On définit l'apparence de la part "active"
-        insidetextorientation='horizontal'
-    )
+    fig2.update_traces(marker_line_color='white', marker_line_width=1, opacity=0.9)
 
-    fig.update_layout(
-    # Effet de transition fluide
-        transition_duration=500,
-        showlegend=True
-    )
+    fig2.update_layout(xaxis_tickangle=0)
     
     st.plotly_chart(fig2, use_container_width=True)
             
