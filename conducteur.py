4d15d22b6dbbd7bcb2fc57111d72b2569677cea3
kferchichi-lab <k.ferchichi@tpr.com.tn>
2026-05-15 12:41:13 +0100

    Update conducteur.py

--- conducteur.py
+++ conducteur.py
@@ -277,9 +277,15 @@ with tab_stats:
             
             # Renommer pour l'affichage
     tableau_somme.columns = ['Code Cause', 'Temps Total (Minutes)']
+    st.dataframe(
+        tableau_somme, 
+        use_container_width=False, 
+        hide_index=True,
+        width=400  # Vous pouvez ajuster cette valeur (en pixels) selon votre besoin
+    )
             
             # Affichage du tableau (hide_index=True pour enlever les chiffres 0, 1, 2 à gauche)
-    st.table(tableau_somme)        
+      
         # Petit indicateur visuel pour le total général
     total_general = tableau_somme['Temps Total (Minutes)'].sum()
     st.metric("TOTAL GÉNÉRAL DES ARRÊTS", f"{total_general} min")
