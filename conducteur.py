0a12d232212676e1e1b1d268c5dd071fd4e97e1c
kferchichi-lab <k.ferchichi@tpr.com.tn>
2026-05-18 09:19:14 +0100

    Update conducteur.py

--- conducteur.py
+++ conducteur.py
@@ -256,8 +256,23 @@ with tab_stats:
                          hole=0.4, # Pour en faire un Donut chart (plus moderne)
                          color_discrete_sequence=px.colors.qualitative.Pastel)
             
-            fig.update_traces(textposition='inside', textinfo='percent+label')
-            st.plotly_chart(fig, use_container_width=True)
+          fig.update_traces(
+        textposition='inside', 
+        textinfo='percent'  # <--- On a supprimé '+label' pour n'avoir que le %
+    )
+    
+    # Ajustement de la légende pour qu'elle soit bien lisible à droite
+    fig.update_layout(
+        legend=dict(
+            orientation="v",
+            yanchor="middle",
+            y=0.5,
+            xanchor="left",
+            x=1.05
+        )
+    )
+    
+    st.plotly_chart(fig, use_container_width=True)
             
             # Optionnel : Répartition du temps d'arrêt
             st.divider()
