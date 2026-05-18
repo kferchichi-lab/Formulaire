b4125257a1986d3ceeb9b78e681712b972bd4d22
kferchichi-lab <k.ferchichi@tpr.com.tn>
2026-05-18 10:26:02 +0100

    Update conducteur.py

--- conducteur.py
+++ conducteur.py
@@ -182,13 +182,6 @@ with tab_base:
         df_affichage = pd.read_csv(DB_FILE, sep=";")
         df_affichage['Date'] = df_affichage['Date'].astype(str).str[:10]
 
-        edited_df = st.data_editor(
-            df, 
-            use_container_width=True, 
-            num_rows="dynamic",  # Active l'ajout/suppression de lignes
-            key="data_editor_key"
-        )
-
         
         colonnes_visibles = ['Date', 'Presse', 'Poste', 'Filiere', 'Lopin', 'Duree_Min', 'Cause']
         df_pour_affichage = df_affichage[[c for c in colonnes_visibles if c in df_affichage.columns]]
