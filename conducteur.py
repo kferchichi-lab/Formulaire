79058ac17ec8fc2b4bb1a1b6e767e836b53eb5f9
kferchichi-lab <k.ferchichi@tpr.com.tn>
2026-06-23 15:27:14 +0100

    Update conducteur.py

--- conducteur.py
+++ conducteur.py
@@ -138,10 +138,7 @@ with st.sidebar:
     """)
     st.warning("⚠️ Tolérance : +/- 10°C")
 
-col_logo, col_titre = st.columns([1, 5])
-with col_logo:
     st.image("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR6q1BtDSDgVnJZFo0hOBfQJoDS6OYiub-qfQ&s", width=150)
-with col_titre:
     st.markdown("## Tunisie Profilés d'Aluminium")
     st.markdown("#### Direction Maintenance et Travaux Neufs")
 st.divider()
